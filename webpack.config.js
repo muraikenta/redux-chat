f3c3efb9aeaa0412f8311651a0bd690602927c2e
村井謙太 <muraikenta0507@gmail.com>
2017-04-23 18:57:02 +0900

    Add js configs

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+module.exports={
+  entry: ["./app/assets/javascripts/src/app.js"],
+  output: {
+    path: __dirname + "/public/scripts",
+    filename: "bundle.js"
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: ["react", "es2015", "stage-0"]
+        }
+      }
+    ]
+  }
+}
