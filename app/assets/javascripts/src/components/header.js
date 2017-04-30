3d27066e296a823bc1d60cd457a18168c047aab7
村井謙太 <muraikenta0507@gmail.com>
2017-04-30 10:51:38 +0900

    Update header ui using react-bootstrap

--- app/assets/javascripts/src/components/header.js
+++ app/assets/javascripts/src/components/header.js
@@ -1,21 +1,24 @@
 import React from 'react'
+import {Navbar, Nav, NavItem} from 'react-bootstrap'
 import HttpRequest from '../lib/http_request'
 
 class Header extends React.Component {
   render() {
     const {userName} = this.props
     return (
-      <header className='header'>
-        <h1>ChatApp</h1>
-        <ul>
-          <li>{userName}</li>
-          <li>
-            <a onClick={() => {HttpRequest.delete('/users/sign_out')}}>
-              ログアウト
-            </a>
-          </li>
-        </ul>
-      </header>
+      <Navbar>
+        <Navbar.Header>
+          <Navbar.Brand>
+            <a href="#">ReactChat</a>
+          </Navbar.Brand>
+        </Navbar.Header>
+        <Nav pullRight>
+          <NavItem>{userName}</NavItem>
+          <NavItem onClick={() => {HttpRequest.delete('/users/sign_out')}}>
+            ログアウト
+          </NavItem>
+        </Nav>
+      </Navbar>
     )
   }
 }
